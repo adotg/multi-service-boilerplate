e4636b2e02cda78872596571145aead69e2bbcab
akash <akash00goswami@gmail.com>
2021-07-10 18:10:28 +0530

    Consolidated run mechanism

--- data/src/main/java/boilerplate/multiservice/App.java
+++ data/src/main/java/boilerplate/multiservice/App.java
@@ -1,12 +1,35 @@
 package boilerplate.multiservice;
 
-/**
- * Hello world!
- *
- */
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
+
+@SpringBootApplication
+@Slf4j
 public class App {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    private Environment springEnv;
+
+    @Autowired
+    public void setSpringEnv(Environment springEnv) {
+        this.springEnv = springEnv;
+    }
+
+    public static void main( String[] args ) {
+        SpringApplication.run(App.class);
+    }
+
+    @Bean
+    ApplicationRunner applicationRunner()  {
+        return args -> {
+            String[] profiles = springEnv.getDefaultProfiles();
+            for (String profile : profiles) log.warn("[Default Profiles] {}", profile);
+
+            profiles = springEnv.getActiveProfiles();
+            for (String profile : profiles) log.warn("[Active Profiles] {}", profile);
+        };
     }
 }
